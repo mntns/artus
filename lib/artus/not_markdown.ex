a4c741cbaa2830c7f7be38816233a72ab9d391e0
Eddy Shure <github@monoton.space>
2017-09-10 18:34:54 +0200

    Add logo dir

---
+++ lib/artus/not_markdown.ex
@@ -0,0 +1,26 @@
+defmodule Artus.NotMarkdown do
+  def to_html(string) do
+    string
+    |> outer_replace(~r/^>{3}(.+?)\z/sm, "blockquote")
+    |> outer_replace(~r/^>(.+?)$/sm, "blockquote")
+    |> outer_replace(~r/\*(.+?)\*/, "b")
+    |> outer_replace(~r/_(.+?)_/, "i")
+    |> outer_replace(~r/~(.+?)~/, "s")
+    |> outer_replace(~r/`{3}(.+?)`{3}/sm, "pre")
+    |> outer_replace(~r/`(.+?)`/, "code")
+    |> create_lists()
+  end
+
+  defp create_lists(text) do
+    # TODO: list creation (ol/ul)
+    text
+  end
+    
+  defp outer_replace(string, regex, tag) do
+    Regex.replace(regex, string, fn _, x -> build_tag(x, tag) end)
+  end
+
+  defp build_tag(body, tag) do
+    "<#{tag}>" <> body <> "</#{tag}>"
+  end
+end
