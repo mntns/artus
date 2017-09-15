38bc76deebb2596ac5575cd7ca7589b4395352b2
Eddy Shure <github@monoton.space>
2017-09-15 23:08:47 +0200

    Change PageTitle to be more conservative

--- lib/artus/page_title.ex
+++ lib/artus/page_title.ex
@@ -8,7 +8,7 @@ defmodule Artus.PageTitle do
 
 	defp get(%{ view_module: Artus.Admin.PageView }), do: "Admin"
 	defp get(%{ view_module: Artus.EntryView, view_template: "show.html", entry: entry }) do
-		"Entry #" <> "#{entry.id}: " <> entry.titl_title
+    "Entry #" <> "#{entry.id}"
 	end
 	defp get(_), do: nil
 end
