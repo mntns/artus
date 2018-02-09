09a4a59b76799cefa7006b0693ab5f145e088542
Eddy Shure <github@monoton.space>
2018-02-09 08:15:54 +0100

    Add more documentation to DefinitionManager

--- lib/artus.ex
+++ lib/artus.ex
@@ -2,8 +2,6 @@ defmodule Artus do
   @moduledoc false
   use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
@@ -12,22 +10,18 @@ defmodule Artus do
       supervisor(Artus.Endpoint, []),
       # Start the Ecto repository
       supervisor(Artus.Repo, []),
-      # Here you could define other workers and supervisors as children
-      # worker(Artus.Worker, [arg1, arg2, arg3]),
+      
+      # Start workers for artus
       worker(Artus.DefinitionManager, []),
       worker(Artus.EntryCache, []),
       worker(Artus.QueryRunner, []),
       worker(Artus.EventLogger, [])
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Artus.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
-  # Tell Phoenix to update the endpoint configuration
-  # whenever the application is updated.
   def config_change(changed, _new, removed) do
     Artus.Endpoint.config_change(changed, removed)
     :ok
--- lib/artus/definition_manager.ex
+++ lib/artus/definition_manager.ex
@@ -2,55 +2,68 @@ defmodule Artus.DefinitionManager do
   @moduledoc "Module for dynamically loading definition JSON files"
   use GenServer
 
+  @doc "Starts definition manager"
   def start_link do
     GenServer.start_link(__MODULE__, :ok, [name: :def_manager])
   end
 
+  @doc "Initializes DefinitionManager by reading definitions"
   def init(:ok) do
     defs = read_defs() |> Map.put(:notice, nil)
     {:ok, defs}
   end
 
-  # Client
+  @doc "Hotswaps definitions by rereading them"
   def reread do
     GenServer.cast(:def_manager, :reread)
   end
 
+  @doc "Get modal information by id"
   def get_modal(id) do
     GenServer.call(:def_manager, {:modal, id})
   end
   
+  @doc "Get input form field information by id"
   def get_field(id) do
     GenServer.call(:def_manager, {:field, id})
   end
 
+  @doc "Get input form fields for all types"
   def fields do
     GenServer.call(:def_manager, :fields)
   end
 
+  @doc "Get all input field definitions"
   def field_defs do
     GenServer.call(:def_manager, :field_defs)
   end
 
+  @doc "Get all languages"
   def languages do
     GenServer.call(:def_manager, :languages)
   end
 
+  @doc "Get all options"
   def options do
     GenServer.call(:def_manager, :options)
   end
 
+  @doc "Get all branches"
   def branches do
     GenServer.call(:def_manager, :branches)
   end
 
+  @doc "Sets public notice"
   def set_notice(notice) do
     GenServer.cast(:def_manager, {:set_notice, notice})
   end
+
+  @doc "Gets public notice"
   def get_notice do
     GenServer.call(:def_manager, :get_notice)
   end
 
+  @doc false
   def handle_cast(:reread, _state) do
     {:noreply, read_defs()}
   end
@@ -58,7 +71,7 @@ defmodule Artus.DefinitionManager do
     {:noreply, %{state | :notice => notice}}
   end
 
-
+  @doc false
   def handle_call(:fields, _from, state) do
     {:reply, state.fields, state}
   end
@@ -74,18 +87,17 @@ defmodule Artus.DefinitionManager do
   def handle_call(:branches, _from, state) do
     {:reply, state.branches, state}
   end
-
   def handle_call({:modal, id}, _from, state) do
     {:reply, state.modals[id], state}
   end
   def handle_call({:field, id}, _from, state) do
     {:reply, state.field_defs[id], state}
   end
-
   def handle_call(:get_notice, _from, state) do
     {:reply, state.notice, state}
   end
 
+  @doc "Reads definitions from JSON files in `defs/` and returns map"
   def read_defs do
     ~w(modals field_defs fields languages options branches)
     |> Enum.map(fn(x) -> {String.to_atom(x), "defs/#{x}.json" |> File.read! |> JSX.decode!} end) 
