3ef77950a429e0146cd5a7f236988e5c3354bf6d
Eddy Shure <github@monoton.space>
2018-12-18 12:41:54 +0100

    Make minor style changes

--- web/views/error_view.ex
+++ web/views/error_view.ex
@@ -3,13 +3,12 @@ defmodule Artus.ErrorView do
 
   def render("404.html", _assigns) do
     # render "404.html"
-    render(__MODULE__, "not_found.html")
+    render("not_found.html")
     # , layout: {Artus.LayoutView, "app.html"})
   end
 
   def render("500.html", _assigns) do
-    "test"
-    # render "500.html.eex"
+    render "internal_error.html"
   end
 
   # In case no render clause matches or no
